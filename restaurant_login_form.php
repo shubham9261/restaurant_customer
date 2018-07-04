b30dfbb5014958ed912afbf50d5ff3d75acf92bf
Ranit Ganai <ranitkat98@gmail.com>
2018-07-05 00:39:08 +0530

    login page  commited

---
+++ restaurant_login_form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+     <head>
+        <link rel="stylesheet" href="css/restaurant_login_form.css"/>
+     </head>
+     <body background="img/coffee.jpg">  
+        <div id="main_block">
+            <form method="post" action="login_submit.php">
+            <font>Email</font><br/>
+            <input type="text" name="email"/></br></br></br>
+            <font>Password</font><br/>
+            <input type ="password" name="password"/>
+            <br/><br/><br/><br/>
+            <input type="submit" value="Sign in"/>
+            </form>
+        <div/> 
+    </body>
+</html>   
