ca20d68b734a4d07cc5b964e2030554f1a799760
Anikeet Dey <anikeetdey3@gmail.com>
2018-07-09 23:06:55 +0530

    mylogin form

---
+++ mylogin_form.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE HTML>
+<html> 
+ <head>
+  <link rel="stylesheet" href="css/style.css"/>
+    </head>
+  <body background="f2.jpg">
+     <h1> LOGIN FORM</h1>
+
+<form action="mylogin_submit.php" method="post">
+E-mail: <input type="text" name="email"><br>
+Password:<input type="password" name="password"><br>
+<input type="submit"><br>
+
+</form>
+
+</body>
+</html>
+
