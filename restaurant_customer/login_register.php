6b12f2c3f2be33b94bb0c04b0bfa5d75593c671a
Ranit Ganai <ranitkat98@gmail.com>
2018-07-09 14:26:20 +0530

    firstpage commit

---
+++ restaurant_customer/login_register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+     <head>
+        <link rel="stylesheet" href="css/bootstrap.css"/>
+     </head>
+     
+     <body background="img/coffee.jpg">  
+        <div class="container">
+                <div class="row align-items-center">
+                  <div class="mx-auto">
+                      <a href="restaurant_login_form.php" class="btn btn-info" role="button">Login</a>
+                      <a href="restaurant_register_form.php" class="btn btn-info" role="button">Register</a>
+                  <div/>
+              <div/>
+        <div/>
+     <body/>
+<html/>
